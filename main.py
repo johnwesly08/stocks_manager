448e3e1637a1d6c4aef4a283312fa795da1a0429
johnwesly08 <johnwesleywork08@gmail.com>
2023-05-15 16:32:28 +0530

    login and register page complelted

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-from 
+from flask import Flask, redirect,render_template,request
+
+app = Flask("__name__")
+
+
+
+@app.route("/")
+def signin():
+    return render_template("com_login.html")
+
+
+if __name__ == "__main__":
+    app.run()
