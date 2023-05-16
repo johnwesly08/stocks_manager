06301eecdfa666fced3e8650accf0ca9b4918f49
johnwesly08 <johnwesleywork08@gmail.com>
2023-05-16 10:49:21 +0530

    all the user and login, regostraion pages ui done.

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-from flask import Flask, redirect,render_template,request
+from flask import Flask, redirect,render_template,request,url_for
 
 app = Flask("__name__")
 
 
 
 @app.route("/")
-def signin():
+def main():
     return render_template("com_login.html")
+
+@app.route("/dash")
+def home():
+    return render_template("dashboard.html")
+
+@app.route("/login")
+def login():
+    return render_template("com_login.html")
+
+@app.route("/register")
+def register():
+    return render_template("com_register.html")
+
+@app.route("/user_login")
+def user():
+    return render_template("user_login.html")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
