1bf9db1475994fbc3b51e624a6ed526c83a5c02e
abhinesh-ramasamy <abhineshpr101@gmail.com>
2023-05-16 16:08:54 -0700

    Worked MySQL for Sign Up page

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from flask import Flask, redirect,render_template,request,url_for
+from flask_mysqldb import MySQL
 
 app = Flask("__name__")
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Pragathi@916'
+app.config['MYSQL_DB'] = 'stocks'
+app.secret_key="Resource_Control_And_Optimization_System"
+mysql = MySQL(app)
 
 
 
@@ -14,10 +22,26 @@ def home():
 
 @app.route("/login")
 def login():
+    c_id = request.form['company_id']
+    c_mail = request.form['business_email']
+    c_pass = request.form['c_password']
     return render_template("com_login.html")
 
-@app.route("/register")
+@app.route("/register", methods = ['POST','GET'])
 def register():
+    if request.method == 'POST':
+        com_name = request.form['company_name']
+        com_id = request.form['company_id']
+        com_dir = request.form['director']
+        com_pass = request.form['set_password']
+        com_mail = request.form['business_email']
+        com_country = request.form['country']
+        com_state = request.form['state']
+        con = mysql.connection.cursor()
+        con.execute("INSERT INTO companies (Company_Name, Company_ID, Director, Company_Password, Business_Email, Country, State) VALUES (%s,%s,%s,%s,%s,%s,%s)",(com_name,com_id,com_dir,com_pass,com_mail,com_country,com_state))
+        con.connection.commit()
+        con.close()
+        return redirect(url_for('/login'))
     return render_template("com_register.html")
 
 @app.route("/user_login")
